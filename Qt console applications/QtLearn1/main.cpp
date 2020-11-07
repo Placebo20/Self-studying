8750acc40163188120e063a72b13c1c2c35ffc4e
Placebo20 <dimag586@gmail.com>
2020-11-07 16:14:34 +0200

    first commit

---
+++ Qt console applications/QtLearn1/main.cpp
@@ -0,0 +1,26 @@
+#include <QTextStream>
+
+int main(int argc, char *argv[])
+{
+    //Different options for initializing QString
+    /*QTextStream out(stdout);
+
+    QString str1 = "The night train";
+    out << str1 << Qt::endl;
+
+    QString str2("A yellow rose");
+    out << str2 << Qt::endl;
+
+    std::string s1 = "A blue sky";
+    QString str3 = s1.c_str();
+    out << str3 << Qt::endl;
+
+    std::string s2 = "A thick fog";
+    QString str4 = QString::fromLatin1(s2.data(),s2.size());
+    out << str4 << Qt::endl;
+
+    char s3[] = "A deep forest";
+    QString str5(s3);
+    out << str5 << Qt::endl;*/
+
+}
